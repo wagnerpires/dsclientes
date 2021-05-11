3ff4579aff1fd6310fcff5bd8cf27b42d944864f
wagnerpires@hotmail.com <wagnerpires@hotmail.com>
2021-05-11 12:30:17 -0300

    H2 Adicionado e configurado

--- src/main/java/com/wrtecnologia/dsclientes/domain/Client.java
+++ src/main/java/com/wrtecnologia/dsclientes/domain/Client.java
@@ -3,10 +3,19 @@ package com.wrtecnologia.dsclientes.domain;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Client implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String name;
 	private String cpf;
 	private Double income;
