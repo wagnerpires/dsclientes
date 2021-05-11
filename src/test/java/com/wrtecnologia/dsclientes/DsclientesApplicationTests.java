bb0d7c499b8756f025d27bc6011ad2eb46f88141
wagnerpires@hotmail.com <wagnerpires@hotmail.com>
2021-05-10 23:01:42 -0300

    Commit Inicial

---
+++ src/test/java/com/wrtecnologia/dsclientes/DsclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrtecnologia.dsclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
